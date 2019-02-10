9f1927808a73a30409830772b90b3c21335b9275
Andrei Bogomja <bogomja@gmail.com>
2019-02-10 05:26:05 +0000

    Move field 'year' to the top

--- src/main/java/com/otus/hw_06/domain/Book.java
+++ src/main/java/com/otus/hw_06/domain/Book.java
@@ -20,6 +20,9 @@ public class Book {
     @Column(name = "title")
     private String title;
 
+    @Column(name = "written")
+    private String year;
+
     @ManyToMany(
         cascade = {CascadeType.PERSIST, CascadeType.MERGE}
     )
